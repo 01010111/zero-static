86039d974cf75c220d43b7152d3facc554ef7802
01010111 <will.blanton.804@gmail.com>
2021-10-08 21:31:58 -0400

    break out content

--- Content.hx
+++ Content.hx
@@ -1,5 +1,64 @@
-var index_html = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t%% component header.html\n</head>\n<body>\n\t%% markdown index-content.md\n</body>\n</html>';
-var global_css = '* {\n\tmargin: 0;\n\tpadding: 0;\n\t-moz-box-sizing: border-box; \n\t-webkit-box-sizing: border-box; \n\tbox-sizing: border-box; \n}';
-var header_html = '<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>Hello World</title>\n<link rel="stylesheet" href="style.css">';
-var index_content_md = '# Hello World\n\nThis is my new website!';
-var deploy_yml = "name: Build and Deploy\non: [push]\njobs:\nbuild-and-deploy:\n\truns-on: ubuntu-latest\n\tsteps:\n\t- name: Checkout 👀\n\t\tuses: actions/checkout@v2.3.1\n\n\t- name: Setup Haxe 🚧\n\t\tuses: krdlab/setup-haxe@v1\n\n\t- name: Setup libs 📚\n\t\trun: |\n\t\thaxelib git zero-static https://github.com/01010111/zero-static --always --quiet\n\t\thaxelib install markdown --always --quiet\n\n\t- name: Build 🔨\n\t\trun: |\n\t\thaxe -lib zero-static -lib markdown\n\n\t- name: Push changes 🏃‍♀️\n\t\tuses: ad-m/github-push-action@master\n\t\twith:\n\t\tgithub_token: ${{ secrets.GITHUB_TOKEN }}\n\t\tbranch: ${{ github.ref }}\n\t\tforce: true\n\t\n\t- name: Deploy to GitHub Pages 🚀\n\t\tuses: Cecilapp/GitHub-Pages-deploy@v3\n\t\tenv:\n\t\tGITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}\n\t\twith:\n\t\temail: me@mysite.com\n\t\tbuild_dir: dist\n\t\tjekyll: no";
+var index_html = '<!DOCTYPE html>
+<html lang="en">
+<head>
+	%% component header.html
+</head>
+<body>
+	%% markdown index-content.md
+</body>
+</html>';
+
+var global_css = '* {
+	margin: 0;
+	padding: 0;
+	-moz-box-sizing: border-box; 
+	-webkit-box-sizing: border-box; 
+	box-sizing: border-box; 
+}';
+
+var header_html = '<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Hello World</title>
+<link rel="stylesheet" href="style.css">';
+
+var index_content_md = '# Hello World
+
+This is my new website!';
+
+var deploy_yml = "name: Build and Deploy
+on: [push]
+jobs:
+build-and-deploy:
+  runs-on: ubuntu-latest
+  steps:
+  - name: Checkout 👀
+    uses: actions/checkout@v2.3.1
+
+  - name: Setup Haxe 🚧
+    uses: krdlab/setup-haxe@v1
+
+  - name: Setup libs 📚
+    run: |
+    haxelib git zero-static https://github.com/01010111/zero-static --always --quiet
+    haxelib install markdown --always --quiet
+
+  - name: Build 🔨
+    run: |
+    haxe -lib zero-static -lib markdown
+
+  - name: Push changes 🏃‍♀️
+    uses: ad-m/github-push-action@master
+    with:
+    github_token: ${{ secrets.GITHUB_TOKEN }}
+    branch: ${{ github.ref }}
+    force: true
+  
+  - name: Deploy to GitHub Pages 🚀
+    uses: Cecilapp/GitHub-Pages-deploy@v3
+    env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    with:
+    email: me@mysite.com
+    build_dir: dist
+    jekyll: no";
