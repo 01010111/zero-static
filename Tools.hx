8624126c5d976b7b2a2646b0a4f39f363c84faff
Will Blanton <will.blanton.804@gmail.com>
2023-04-12 16:52:39 -0400

    v2

--- Tools.hx
+++ Tools.hx
@@ -1,3 +1,8 @@
 function last_index<T>(arr:Array<T>):T {
 	return arr[arr.length-1];
 }
+
+function shift_arr_return<T>(arr:Array<T>):Array<T> {
+	if (arr.length < 2) return [];
+	return [for (i in 1...arr.length) arr[i]];
+}
